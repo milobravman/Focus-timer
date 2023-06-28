aa170ab968ba6142c1bcb1b44f5412bc0eab5792
milolynch <milobravman20@gmail.com>
2023-06-28 16:19:59 -0400

    removing block returns you to website

--- Timer-with-Popup-new-feature-sandbox/index.js
+++ Timer-with-Popup-new-feature-sandbox/index.js
@@ -6,13 +6,31 @@ const list = document.getElementById("block-list")
 chrome.declarativeNetRequest.getDynamicRules().then((res)=>{
     //console.log(res)
     res.forEach((score) => {
-        console.log(score.condition.urlFilter);
+        console.log(score);
         let temp = score.condition.urlFilter.match(myReg)
         //console.log(temp[1])
         let listItem = document.createElement("li")
-        listItem.innerHTML=listItem.innerHTML + temp[1];
+        let removedListItem = document.createElement('button')
+        removedListItem.id = score.id;
+        removedListItem.addEventListener(
+            'click',
+            function (){
+                handleRemove(score.id, score.condition.urlFilter)
+            }
+        )
+        removedListItem.innerHTML = "remove"
+        listItem.innerHTML=listItem.innerHTML + temp[1]
+        listItem.appendChild(removedListItem)
         list.appendChild(listItem)
     });
-
 })
 
+function handleRemove(id, dest) {
+    //document.getElementById(id)
+    chrome.declarativeNetRequest.updateDynamicRules({
+        removeRuleIds: [id] 
+    }).then(() => {
+        console.log("attempting to reload link")
+        location.href = dest
+    })
+}
