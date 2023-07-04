7ff2f438bc4ae92a7a9d84cc8c337fca433bb370
milolynch <milobravman20@gmail.com>
2023-07-03 23:20:19 -0400

    working on options

---
+++ Timer-with-Popup-new-feature-sandbox/options.js
@@ -0,0 +1,40 @@
+// display the blocked list for  the user
+
+ 
+
+documentnt.getElementById('add-to-block-list').addEventListener("click", handleAddtoBlock)
+
+const myReg = /^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n?]+)/
+const list = document.getElementById("block-list")
+
+chrome.declarativeNetRequest.getDynamicRules().then((res)=>{
+    //console.log(res)
+    res.forEach((score) => {
+        console.log(score);
+        let temp = score.condition.urlFilter.match(myReg)
+        //console.log(temp[1])
+        let listItem = document.createElement("li")
+        let removedListItem = document.createElement('button')
+        removedListItem.id = score.id;
+        removedListItem.addEventListener(
+            'click',
+            function (){
+                handleRemove(score.id, score.condition.urlFilter)
+            }
+        )
+        removedListItem.innerHTML = "remove"
+        listItem.innerHTML=listItem.innerHTML + temp[1]
+        listItem.appendChild(removedListItem)
+        list.appendChild(listItem)
+    });
+})
+
+function handleRemove(id, dest) {
+    //document.getElementById(id)
+    chrome.declarativeNetRequest.updateDynamicRules({
+        removeRuleIds: [id] 
+    }).then(() => {
+        //console.log("attempting to reload link")
+        //location.href = dest
+    })
+}
