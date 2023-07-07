af7727d3fc2b0f40bc7e5b03d26355f98271ab77
milolynch <milobravman20@gmail.com>
2023-07-07 17:24:34 -0400

    new extension folder
    build back up from "timer with popup" and some of the additional features that were made in the sandbox, ("extension does run on this page") but adding robber's default suggestions and improving the UI and UX of the application

---
+++ Refined-agency-assistant/options/options.js
@@ -0,0 +1,150 @@
+// display the blocked list for  the user
+ 
+const list = document.getElementById("block-list")
+
+document.getElementById('submit-block-input').addEventListener('click', handleSubmitBlock)
+
+document.getElementById('add-to-block-list').addEventListener("click", handleShowAddToBlock)
+
+
+// it may be poor practice to have two different functions make rules since this and the function in popup.js
+// the other option would be to make these functions send messages to the service worker, background.js and have the rules made there
+// the downside would be like likely increase in time it would take for a rule to be added since there would be the extra step involved
+function handleSubmitBlock() {
+    let input = document.getElementById('block-input')
+    try {
+        let url = new URL(input.value)
+        let newRule = {};
+        chrome.declarativeNetRequest.getDynamicRules().then((res) => {
+            let newID = 0
+            res.forEach((rule) => {
+                newID = newID + rule.id
+            })
+            newRule.id = newID+1 // collection still need +1 incase 1 or zero elements in the list 
+            newRule.priority = 1
+            newRule.action = {
+                "type": "redirect",
+                "redirect": {
+                  "extensionPath": "/blocked-page/index.html"
+                }
+              }
+            newRule.condition = {
+                "urlFilter": url.origin,
+                "resourceTypes": ["main_frame"]
+            }
+            chrome.declarativeNetRequest.updateDynamicRules({
+                addRules:[newRule]
+            })
+            let listItem = document.createElement("li")
+            let removedListItem = document.createElement('button')
+            removedListItem.id = newID+1
+            removedListItem.addEventListener(
+                'click',
+                function (){
+                    handleRemove(newID+1)
+                }
+            )
+            removedListItem.innerHTML = "remove"
+            listItem.innerHTML=listItem.innerHTML + url.host
+            listItem.appendChild(removedListItem)
+            list.appendChild(listItem)
+            input.value = ''
+        })
+        // html addition
+    } catch (error) {
+        console.log("not a valid URL")
+        input.value = ''
+        let errorMessage = document.getElementById('block-input-label')
+        errorMessage.innerHTML = 'copy url of site to block, Ex "https://www.youtube.com"'
+        errorMessage.style.color = 'red'
+        //console.log(errorMessage)
+    }
+}
+
+// Real time dom manipulation
+function handleShowAddToBlock() {
+    let hidden = document.getElementsByClassName('b-i')
+    for (let i = 0; i< hidden.length; i++) {
+        hidden[i].style.display="inline";
+    }
+    document.getElementById('add-to-block-list').style.display='none'
+}
+
+//deletes a rule
+function handleRemove(id) {
+    chrome.declarativeNetRequest.updateDynamicRules({
+        removeRuleIds: [id] 
+    }).then(() => {
+        let toRemove = document.getElementById(id)
+        toRemove.parentElement.remove()
+    })
+}
+
+//Populates the page with the blocked websites
+//Not wrapped in a function since I want it called every time the page loads
+chrome.declarativeNetRequest.getDynamicRules().then((res)=>{
+    res.forEach((score) => {
+        console.log(score);
+        let url = new URL(score.condition.urlFilter)
+        let listItem = document.createElement("li")
+        let removedListItem = document.createElement('button')
+        removedListItem.id = score.id;
+        removedListItem.addEventListener(
+            'click',
+            function (){
+                handleRemove(score.id)
+            }
+        )
+        removedListItem.innerHTML = "remove"
+        listItem.innerHTML=listItem.innerHTML + url.host
+        listItem.appendChild(removedListItem)
+        list.appendChild(listItem)
+    });
+})
+
+/*
+How it could work
+User goes to restricted page
+Timer starts counting to the limit
+If the limit is reached
+The blocking rule is added
+A timestamp is created for when this rule should be automatically removed
+When the service worker loads it needs to check if there are any restricted websites actively blocked and if the should be unblocked
+If the use leaves the page before the limit is reached
+The time resets
+This opens a loophole of leaving and coming back to the page
+so maybe the # of visits to a restricted page should also call for blocking
+*/
+
+document
+    .getElementById("submit-restrict-input")
+    .addEventListener("click", checkValidURL)
+
+// handle Submit Restrict functions
+
+function checkValidURL() {
+    try {
+    let url = new URL(document
+        .getElementById("restrict-input-url").value)
+    let timeToAllow = document
+        .getElementById("restrict-input-time-using").value
+    let timeToBlock = document
+        .getElementById("restrict-input-time-cooldown").value
+    addToLocalStorage(url, timeToAllow, timeToBlock)
+    } catch (error) {
+        console.log('nope');
+        // add error message for user
+    }
+}
+
+function addToLocalStorage(url, timeA, timeB) {
+    let existing = []
+    chrome.storage.local.get("rules").then((res) => {
+        console.log(res)
+    })
+    chrome.storage.local.set({rules: [{
+        url: url.origin,
+        timeSiteAllowed: timeA,
+        timeSiteBlocked: timeB
+    }]})
+}
---
+++ Refined-agency-assistant/popup/popup.js
@@ -0,0 +1,229 @@
+/* Popup.js 
+handles the functionality of the popup page which includes
+Setting a timer
+Changing pages
+Blocking websites
+*/
+
+document.getElementById('to-options-page').addEventListener('click', handleOptions)
+document.getElementById('block-button').addEventListener('click', handlePageBlock)
+
+const startTimer = document.getElementById('startTimer');
+const goToPageTwo = document.getElementById('page-2-button')
+const goToPageOne = document.getElementById("page-1-button")
+
+// begins a sequence of functions that 
+// taking in user input
+// run some verifications
+// send data to other parts of the extension
+// create a real time counter
+startTimer.addEventListener(
+    'click',
+    wrapper
+    )
+
+
+goToPageTwo.addEventListener(
+    'click',
+    makePage2Visible
+    )
+
+goToPageOne.addEventListener(
+    'click',
+    makePage1Visible
+    )
+
+checkAccessiblePage()
+checkExisting()
+
+// creates a new tab at the options.html
+function handleOptions() {
+    chrome.tabs.create({
+        url: "/options/options.html"
+    })
+}
+    
+// get the active tab and get the url
+// then add it to the dynamic rules
+// then refresh or redirect form the page
+function handlePageBlock() {
+    chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+        //console.log(tabs[0].id)
+        let newRule = {};
+        chrome.declarativeNetRequest.getDynamicRules().then((res) =>{
+            //console.log(res)
+            let urlParts = new URL(tabs[0].url)
+            let newID = 0
+            res.forEach((rule) => {
+                newID = newID + rule.id
+            })
+            newRule.id = newID+1
+            newRule.priority = 1
+            newRule.action = {
+                "type": "redirect",
+                "redirect": {
+                  "extensionPath": "/blocked-page/index.html"
+                }
+              }
+            newRule.condition = {
+                "urlFilter": urlParts.origin,
+                "resourceTypes": ["main_frame"]
+            }
+            chrome.declarativeNetRequest.updateDynamicRules({
+                addRules:[newRule]
+            }).then((res) =>
+                {
+                    if (tabs[0].id !=undefined){
+                        console.log("the tabid is "+tabs[0].id)
+                        chrome.scripting.executeScript({
+                            files:["/scripts/refresh.js"],
+                            target:{tabId: tabs[0].id}
+                          })
+                    }
+                }
+            )
+        })
+      });
+}
+
+// check accessible url. this will check to see if this is a sight that the extension can access and block any features if not
+function checkAccessiblePage () {
+    chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+        if (tabs[0].url === undefined){
+            let page = document.getElementsByTagName('div')
+            for (let i = 0; i< page.length; i++) {
+                page[i].innerHTML=""
+            }
+            const notAvailable = document.createElement("span")
+            notAvailable.innerHTML ="Not available on this page"
+            let title = document.getElementsByTagName('div')
+            document.body.insertBefore(notAvailable, title[0])
+        } 
+      });
+}
+
+//html style swapping
+function makePage2Visible() {
+    let pageOneElements = document.getElementsByClassName("page-1")
+    for (let i = 0; i< pageOneElements.length; i++) {
+        pageOneElements[i].style.display="none";
+    }
+    
+    let pageTwoElements = document.getElementsByClassName("page-2")
+    for (let i = 0; i< pageTwoElements.length; i++) {
+        pageTwoElements[i].style.display="inline";
+    }
+}
+
+//html style swapping
+function makePage1Visible() {
+    let pageOneElements = document.getElementsByClassName("page-1")
+    for (let i = 0; i< pageOneElements.length; i++) {
+        pageOneElements[i].style.display="inline";
+    }
+    
+    let pageTwoElements = document.getElementsByClassName("page-2")
+    for (let i = 0; i< pageTwoElements.length; i++) {
+        pageTwoElements[i].style.display="none";
+    }
+}
+
+
+// gets the current open tab
+function wrapper() {
+    document.getElementById("timer-input").style.display = "none";
+    document.getElementById("startTimer").style.display = "none";
+    const getTab = new Promise((resolve, reject) => {
+        chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+            console.log("hi")
+            console.log(tabs[0].url)
+            let id = tabs[0].id
+            resolve(id)
+          });
+    })
+    getTab.then((sm) => {
+        getTime(sm)
+    })    
+}
+
+// checks local storage to see if alarm exist
+// this way users can see the timer in real time if they wish
+function checkExisting() {
+    chrome.storage.local.get(["stop"]).then((result) => {
+        if (Number.isSafeInteger(result.stop))// this is checks if a alarm exists
+        {  
+            let timeLeft = result.stop - Date.now() // because date is tract in ms the current date is always smaller than a future date until 2038 
+            if (timeLeft > 0) {
+                document.getElementById("timer-input").style.display = "none";
+                document.getElementById("startTimer").style.display = "none";
+                timeWrapper(timeLeft/60_000)
+            }
+        }
+    })
+}
+
+// creates a realtime counter that shows up in popup.html
+function timeWrapper(time) {
+    let dummyTimer = setInterval(dumTimer, 1000)
+    let countDown = document.getElementById("count-down")
+    countDown.style.display = "inline"
+    setTimeout(() => {
+        console.log("clear interval should fire")
+        clearInterval(dummyTimer)
+    }, (time)* 60_000);
+
+    let total_sec = Math.floor(time * 60)
+
+    function dumTimer() {
+        total_sec -=1
+        let hours = Math.floor(total_sec/(60*60))
+        let mins = Math.floor(total_sec/60)%60
+        let sec = total_sec%60
+
+        if (hours === 0) {
+            hours = "00"
+        }
+        
+        if (hours < 10 && hours > 0){
+            hours = '0'+hours 
+        }
+        
+        if (mins === 0) {
+            mins = "00"
+        }
+        
+        if (mins < 10 && mins > 0){
+            mins = '0'+mins 
+        }
+        
+        
+        if (sec === 0) {
+            sec = "00"
+        }
+        
+        if (sec < 10 && sec > 0){
+            sec = '0'+sec 
+        }
+        
+        let p_time = hours + ":" + mins+ ":" + sec
+        countDown.innerHTML = p_time
+    }
+}
+
+// gets the user inputted value 
+function getTime(id) {    
+    let time = parseInt(document.getElementById('timer-input').value)
+    if (Number.isSafeInteger(time)){
+        sendTime(time , id)
+    }
+}
+
+// sends the TabID and the time in minutes to the background.js
+function sendTime(time, id) {
+    const message = {}
+    message.tabId = id
+    message.time = time
+    const sending = chrome.runtime.sendMessage(message)
+    sending
+    timeWrapper(time)
+}
